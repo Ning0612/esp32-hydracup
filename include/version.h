ef52966b94986c7bb2897e32074302837141a42f
Ning0612 <lance930612@gmail.com>
2026-07-13 04:34:02 +0800

    release: prepare v0.2.0

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define APP_VERSION       "0.1.0"
+#define APP_VERSION       "0.2.0"
 #define APP_VERSION_MAJOR 0
-#define APP_VERSION_MINOR 1
+#define APP_VERSION_MINOR 2
 #define APP_VERSION_PATCH 0
